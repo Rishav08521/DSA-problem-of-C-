bb15d70aa9fe4f80d1443f10d39ef5803f58e884
RISHAV KUMAR <155840057+Rishav08521@users.noreply.github.com>
2024-08-05 03:05:52 +0530

    Add files via upload

---
+++ sumofseries.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the num n:";
+    cin>>n;
+    int ans=0;
+    for(int i=1;i<=n;i++){
+       if(i%2==0){
+        ans-=i;
+       }else{
+        ans+=i;
+       }
+       
+    }
+    cout<<ans<<endl;
+    return 0;
+}
